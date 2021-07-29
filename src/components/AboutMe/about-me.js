55b6ed6269cf4ed0090baa35ce839560a3d49e65
Sir Kam'ron Wilson <sirkamronwilson00@gmail.com>
2021-07-29 14:07:59 -0500

    feat: new about me

--- src/components/AboutMe/about-me.js
+++ src/components/AboutMe/about-me.js
@@ -1,7 +1,17 @@
 import React from "react"
-import { Flex, Text } from "@chakra-ui/react"
+import { Flex, Text, Image, Box, Grid } from "@chakra-ui/react"
+import img from "~images/about-me-pic.png"
+import { FaJava, FaReact, FaJsSquare } from "react-icons/fa"
 
 const AboutMe = () => {
+    const Skills = [
+        { title: "REACT", level: 30 },
+        { title: "REACT NATIVE", level: 10 },
+        { title: "JAVA", level: 50 },
+        { title: "JAVASCRIPT", level: 25 },
+        { title: "WEBPACK", level: 10 },
+    ]
+
     return (
         <Flex h={"90vh"}>
             <Text>It's me, Kam</Text>
